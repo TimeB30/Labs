9a19cd6f49414ef306c13557ec5b319f6b9dcfbe
TimeB <timairisov@gmai..com>
2024-02-23 17:30:23 +0300

    -

---
+++ lab_1/lab_1_1/enums.h
@@ -0,0 +1,16 @@
+enum flags {
+	multiplicity,
+	simp_comp_check,
+	separate,
+	degree_table,
+	summ_numbers,
+	factorial,
+    not_number,
+	wrong_flag,
+    more_than_needs,
+    less_than_needs,
+	no_allocation,
+	too_big_number,
+
+};
+
